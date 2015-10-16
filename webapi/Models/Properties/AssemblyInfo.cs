c804bfd22efbb29b4cbf3945d1736db3b50e2fae
tintoy <tintoy@tintoy.io>
2015-10-17 09:06:13 +1100

    Create initial skeleton for server.

---
+++ webapi/Models/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AkkaInterop.AspNetWebApi.Models")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AkkaInterop.AspNetWebApi")]
+[assembly: AssemblyCopyright("Copyright © Adam Friedman 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("75e1c64d-07e9-4d1b-b81f-c5d3138b3259")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ webapi/Server/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AkkaInterop.AspNetWebApi.Server")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AkkaInterop.AspNetWebApi")]
+[assembly: AssemblyCopyright("Copyright © Adam Friedman 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("1cc0351f-1c56-43cf-aa03-acbbc9f1a4e3")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
